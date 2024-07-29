f38312e625fd291f46e6ca2c6b3466803ce5b300
Trunks2k1ynwa <trungbts3@gmail.com>
2024-07-30 00:06:44 +0700

    push all changes

---
+++ src/components/common/count-icon/count-icon.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import { inter } from '@lib/fonts';
+import { cn } from '@lib/utils';
+import styles from './count-icon.module.scss';
+
+type CountIconProps = {
+  iconElement: ReactNode;
+  count: number;
+  className?: string;
+};
+
+const CountIcon: React.FC<CountIconProps> = ({ iconElement, count, className }) => {
+  return (
+    <div className={cn(styles.countIconWrapper, className)}>
+      {iconElement}
+      <span
+        className={cn('index_ellipse iu-d-flexcenter dark:!bg-primary_light dark:!text-primary_dark', inter.className)}
+      >
+        {count}
+      </span>
+    </div>
+  );
+};
+
+export default CountIcon;
