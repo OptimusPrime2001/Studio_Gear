9f5b6192690224807f5882beb8c5f79558a9cb33
Trunks2k1ynwa <trungbts3@gmail.com>
2024-08-12 22:18:44 +0700

    Sketch ui list product

--- src/app/(main)/shop/page.tsx
+++ src/app/(main)/shop/page.tsx
@@ -1,20 +1,32 @@
 'use client';
 import React from 'react';
 import BreadcrumbPath from '@common/breadcrumb/breadcrumb';
+import ProductCard from '@common/product-card/product-card';
 import FilterIcon from '@components/icons/filter';
 import { Checkbox } from '@components/ui/checkbox';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from '@components/ui/select';
 import { filterCatogories, filterPriceRange } from '@lib/constant';
 import { poppins } from '@lib/fonts';
-import { cn } from '@lib/utils';
+import { cn, uniqueArray } from '@lib/utils';
 import { breadcrumb } from '@mobx/stores/breadcrumStore';
 import styles from './page.module.scss';
 
 const ShopPage = () => {
   React.useLayoutEffect(() => {
-    breadcrumb.updateBreadcrumb({
-      href: 'shop',
-      name: 'Shop'
-    });
+    if (breadcrumb.breadcrumbList.length === 1) {
+      breadcrumb.updateBreadcrumb({
+        href: 'shop',
+        name: 'Shop'
+      });
+    }
   }, []);
   const bannerShop = () => (
     <section className='shop-banner'>
@@ -66,7 +78,43 @@ const ShopPage = () => {
   return (
     <section className={cn(styles.shopPageWrapper, 'media_width_sm')}>
       {bannerShop()}
-      <section className='shop-content'>{filerType()}</section>
+      <section className='shop-content'>
+        {filerType()}
+        <section className='main-content'>
+          <div className='main-content_top'>
+            <h2>Living room</h2>
+            <div>
+              <Select>
+                <SelectTrigger className='w-[180px]'>
+                  <SelectValue placeholder='Sắp xếp theo' />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value='best_seller'>Bán chạy nhất</SelectItem>
+                    <SelectItem value='price_highest'>Giá cao nhất</SelectItem>
+                    <SelectItem value='price_lowest'>Giá thấp nhất</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <div className='display-type'></div>
+            </div>
+          </div>
+          <div className='main-content_product'>
+            {uniqueArray(10).map(item => (
+              <ProductCard
+                name='Loveseat Sofa'
+                price={299000}
+                discount={20}
+                rating={3}
+                isNew
+                href={`/ffffffffff`}
+                imgProduct='https://ucarecdn.com/2df20e1d-9205-4395-9666-2027672517fa/imageplaceholder12x.png'
+                key={item}
+              />
+            ))}
+          </div>
+        </section>
+      </section>
     </section>
   );
 };
