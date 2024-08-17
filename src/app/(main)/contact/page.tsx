d9335c09441726d2919537411514c4e0b21eddc0
Trunks2k1ynwa <trungbts3@gmail.com>
2024-08-17 23:43:04 +0700

    add css for contact page

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import styles from 'contact-page.module.scss';
 
 const ContactPage = () => {
-  return <div>page</div>;
+  return <section className={styles.contactPage}>page</section>;
 };
 export default ContactPage;
