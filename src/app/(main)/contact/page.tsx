d554b0dad82f951fff0cfcca2f37604d508a1504
Trunks2k1ynwa <trungbts3@gmail.com>
2024-08-18 01:50:36 +0700

    change somewhere to update code

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -1,12 +1,89 @@
+'use client';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import LinkButton from '@components/common/link-button/link-button';
 import { Input } from '@components/ui/input';
+import { Textarea } from '@components/ui/textarea';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { listContactUs } from '@lib/constant';
 import { poppins } from '@lib/fonts';
 import Image from 'next/image';
+import { z } from 'zod';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import styles from './contact-page.module.scss';
 
 const ContactPage = () => {
+  const formSchema = z.object({
+    fullname: z.string().min(4, {
+      message: 'Fullname must be at least 4 characters.'
+    }),
+    email: z.string().email({
+      message: 'Please fill the email correct'
+    }),
+    message: z.string()
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullname: '',
+      email: '',
+      message: ''
+    }
+  });
+
+  const formSection = () => {
+    const onSubmit = () => {};
+    return (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='form_section space-y-8'>
+          <FormField
+            control={form.control}
+            name='fullname'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>FULL NAME</FormLabel>
+                <FormControl>
+                  <Input placeholder='Your fullname' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>EMAIL ADDRESS</FormLabel>
+                <FormControl>
+                  <Input type='email' placeholder='Your email' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='message'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>MESSAGE</FormLabel>
+                <FormControl>
+                  <Textarea className='!min-h-[140px]' placeholder='Your message' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit'>Send message</Button>
+        </form>
+      </Form>
+    );
+  };
+  const mapSection = () => {
+    return <div>mapSection</div>;
+  };
   return (
     <section className={styles.contactPage}>
       <h1 className={poppins.className}>We believe in sustainable decor. We’re passionate about life at home.</h1>
@@ -46,10 +123,8 @@ const ContactPage = () => {
         </div>
       </div>
       <div className='form-map_section'>
-        <form action=''>
-          <label htmlFor='Fullname'>FULL NAME</label>
-          <Input />
-        </form>
+        {formSection()}
+        {mapSection()}
       </div>
     </section>
   );
